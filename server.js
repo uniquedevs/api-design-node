5dc6892f9440dcc20b6418731a19144d95094cca
max <maxim.evdokimov@loudclear.com.au>
2018-11-10 07:47:08 +0200

    express

--- server.js
+++ server.js
@@ -1,5 +1,13 @@
 // TODO: create a basic server with express
 // that will send back the index.html file on a GET request to '/'
 // it should then send back jsonData on a GET to /data
+const express = require('express');
+const bodyParser = require('body-parser');
 
-var jsonData = {count: 12, message: 'hey'};
+const app = express();
+
+app.use(express.static('app'));
+
+app.use(bodyParser());
+
+app.listen(3000, () => console.log('listenning port 3000'));
