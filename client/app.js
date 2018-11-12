50522fdc141efe527c4e42a5ca137c30d0178d72
max <maxim.evdokimov@loudclear.com.au>
2018-11-12 22:19:07 +0200

    express

--- client/app.js
+++ client/app.js
@@ -1,9 +1,28 @@
+import _ from 'lodash';
+
 (function(){
     window.onload = () => {
         const form = document.forms.lions;
-        form.addEventListener('submit', () => {
+        window.fetch('/lions/1')
+            .then( res => res.json())
+            .then( json => console.log(json));
+        form.addEventListener('submit', e => {
+            e.preventDefault();
             const formData = new FormData(form);
-
+            window.fetch('/lions', {
+                method: 'post',
+                body: formData,
+            })
+                .then( res => (console.log('Success'), res.json()))
+                .then( json => console.log(json))
+                .catch( err => console.log('Error', err))
+            ;
         });
     };
+    function populateLions(lions){
+        const compiled = _.template('<% _.forEach(lions, function(lion) { %><li><%- lion.name %></li><% }); %>');
+        compiled({ lions });
+        const lionsContainer = document.querySelector('lion-list');
+        lionsContainer.appendChild(compiled);
+    }
 })();
--- server/server.js
+++ server/server.js
@@ -3,22 +3,47 @@
 // it should then send back jsonData on a GET to /data
 const express = require('express');
 const bodyParser = require('body-parser');
+const multer = require('multer');
+const _ = require('lodash');
 
 const app = express();
+const upload = multer();
 
-const employee = [{
-  first_name: 'John',
-  last_name: 'Doe',
-  position: 'CEO'
+const lions = [{
+  id: 1,
+  'name': 'John',
+  'pride': 'Doe',
+  'age': null,
+  'gender': 'male'
 }];
 
 app.use(express.static('client'));
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(upload.array());
 
-app.get('/data', (req, res) => {
-  res.json(employee);
+app.get('/lions', (req, res) => {
+  res.json(lions);
 });
 
+app.post('/lions', (req, res) => {
+  const newLion = req.body;
+  const lastId = _.sortBy(lions, ['id'], ['desc'])[0].id;
+  const newLionWithId = Object.assign(newLion, { id: lastId + 1});
+  lions.push(newLionWithId);
+  res.json(lions);
+});
+
+app.get('/lions/:id', (req, res) => {
+  const lion = _.find(lions, { id: +req.params.id});
+  res.json(lion || {});
+});
+
+app.put('lions/:id', (req, res) => {
+  const lion = req.body;
+  const lionWithId = Object.assign(lion, { id: +req.params.id});
+  Object.assign(lions, lionWithId);
+  res.json(lions);
+});
 app.listen(3000, () => console.log('listenning port 3000'));
